6f70f243fd7ca5bb4163b4255ab296d4e2521bde
Ingram7 <2280037238@qq.com>
2021-05-23 23:15:34 +0800

    first commit

---
+++ demo-simple/client/client.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	pb "go-grpc-example/demo-simple/proto"
+	"log"
+
+	"google.golang.org/grpc"
+)
+
+const PORT = "9001"
+
+func main() {
+	conn, err := grpc.Dial(":"+PORT, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("grpc.Dial err: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewSearchServiceClient(conn)
+	resp, err := client.Search(context.Background(), &pb.SearchRequest{
+		Request: "gRPC",
+	})
+	if err != nil {
+		log.Fatalf("client.Search err: %v", err)
+	}
+	log.Printf("resp: %s", resp.GetResponse())
+}
